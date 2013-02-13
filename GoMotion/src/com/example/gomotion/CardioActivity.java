faca2464a8e9e942cf02c2dd101a300602ede7ac
Jack Hindmarch <jack.hindmarch@gmail.com>
2013-02-13 16:34:11 +0000

    GPS co-ordinates can now be tracked and stored in a list.

--- GoMotion/src/com/example/gomotion/CardioActivity.java
+++ GoMotion/src/com/example/gomotion/CardioActivity.java
@@ -1,13 +1,17 @@
 package com.example.gomotion;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
@@ -15,7 +19,9 @@ import android.view.View;
 
 public class CardioActivity extends Activity 
 {
-	private List<Integer> waypoints;
+	private List<Waypoint> waypoints;
+	private LocationManager locationManager;
+	private GpsStatus gpsStatus;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +46,84 @@ public class CardioActivity extends Activity
 	{
 		view.setEnabled(false);		
 
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		waypoints = new LinkedList<Integer>();
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		waypoints = new LinkedList<Waypoint>();
 		
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, new LocationListener() {
+		locationManager.addGpsStatusListener( new GpsStatus.Listener()
+		{
+			public void onGpsStatusChanged(int event)
+			{
+				gpsStatus = locationManager.getGpsStatus(gpsStatus);
+				
+				switch (event) 
+				{
+			        case GpsStatus.GPS_EVENT_STARTED:
+			            // Do Something with mStatus info
+			            break;
+	
+			        case GpsStatus.GPS_EVENT_STOPPED:
+			            // Do Something with mStatus info
+			            break;
+	
+			        case GpsStatus.GPS_EVENT_FIRST_FIX:
+			            // Do Something with mStatus info
+			            break;
+	
+			        case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+			        	int satCount = 0;			        	
+				        Iterable<GpsSatellite> sats = gpsStatus.getSatellites();
+				        Iterator<GpsSatellite> itr = sats.iterator();
+				        
+				        while(itr.hasNext()) {
+				        	itr.next();
+				        	satCount++;
+				        }				        
+				        
+				        System.out.println(satCount);
+				        break;
+				}
+		    }		
+		});
+		
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
 
 			public void onLocationChanged(Location location) 
 			{
-				System.out.println(location.getLongitude() + ", " + location.getLatitude());
+				waypoints.add(new Waypoint(location.getLongitude(), location.getLatitude()));
 			}
 
-			public void onProviderDisabled(String provider) {
-				
+			public void onProviderDisabled(String provider)
+			{
+				System.out.println("GPS is not turned on.");
 			}
 
-			public void onProviderEnabled(String provider) {
-				
+			public void onProviderEnabled(String provider)
+			{
+				System.out.println("GPS is turned on.");
 			}
 
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-				
+			public void onStatusChanged(String provider, int status, Bundle extras) 
+			{
+				switch (status) 
+				{
+		            case LocationProvider.AVAILABLE:
+		                System.out.println("Network available again\n");
+		                break;
+		            case LocationProvider.OUT_OF_SERVICE:
+		            	System.out.println("Network out of service\n");
+		                break;
+		            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+		            	System.out.println("Network temporarily unavailable\n");
+		                break;
+	            }
 			}			
 		});
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		System.out.println(waypoints);
 	}
 }
---
+++ GoMotion/src/com/example/gomotion/Waypoint.java
@@ -0,0 +1,31 @@
+package com.example.gomotion;
+
+public class Waypoint 
+{
+	private double longitude;
+	private double latitude;
+	
+	public Waypoint(double longitude, double latitude)
+	{
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() 
+	{
+		return longitude;
+	}
+	public void setLongitude(double longitude) 
+	{
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() 
+	{
+		return latitude;
+	}
+	public void setLatitude(double latitude)
+	{
+		this.latitude = latitude;
+	}
+}
